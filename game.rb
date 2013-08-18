01a007df7dde02c6547a3b1f57ac899e981baa4a
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-18 14:43:53 -0400

    added #max function

--- game.rb
+++ game.rb
@@ -1,12 +1,13 @@
-@answer = rand(1000)
+@max = rand(2000)
+@answer = rand(@max)
 
   def can_guess_it
-  question = "Guess a number between 0 and 1000: "
+  question = "Guess a number between 0 and #{@max}: "
   print question
   guess = gets.chomp
   int = guess.to_i
 
-    if /[^0-9]/.match(guess)  || int >= 1001 || int < 0
+    if /[^0-9]/.match(guess)  || int >= (@max + 1) || int < 0
     print " Invalid input, must enter a number between 0 and 1000."
     can_guess_it
     else

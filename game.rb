495435de2fd8151f81c519849ed42a2eadcfe517
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-19 15:30:58 -0400

    refactor elsif

--- game.rb
+++ game.rb
@@ -8,22 +8,17 @@
   int = guess.to_i
 
     if /[^0-9]/.match(guess)  || int >= (@max + 1) || int < 0
-    print " Invalid input, must enter a number between 0 and 1000."
-    can_guess_it
-    else
-      if int < @answer
+      print " Invalid input, must enter a number between 0 and 1000."
+      can_guess_it
+    elsif int < @answer
       print " too low, try again..."
       can_guess_it
-      else
-        if int > @answer
-        print" too high, try again..."
-        can_guess_it
-        else
-          if int = @answer
-          print "Congratulations, you guessed the number."
-          end
-        end
-      end
+    elsif int > @answer
+      print" too high, try again..."
+      can_guess_it
+    elsif int = @answer
+      print "Congratulations, you guessed the number."
     end
   end
+
 can_guess_it

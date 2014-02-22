a85be4ae6a4f29f2648da14fa6e74d5e3e0d6324
Tom Corley <tom.corley@goodmeasures.com>
2014-02-22 02:55:25 -0500

    refactor

--- game.rb
+++ game.rb
@@ -1,24 +1,37 @@
-@max = rand(2000)
-@answer = rand(@max)
-
-  def can_guess_it
-  question = "Guess a number between 0 and #{@max}: "
-  print question
-  guess = gets.chomp
-  int = guess.to_i
-
-    if /[^0-9]/.match(guess)  || int >= (@max + 1) || int < 0
-      print " Invalid input, must enter a number between 0 and 1000."
-      can_guess_it
-    elsif int < @answer
-      print " too low, try again..."
-      can_guess_it
-    elsif int > @answer
-      print" too high, try again..."
-      can_guess_it
-    elsif int = @answer
-      print "Congratulations, you guessed the number."
+def can_guess_it
+  max = rand(1000..2000)
+  answer = rand(max)
+  question = "Guess a number between 0 and #{max}: "
+  guess = question_chomp(question)
+  while guess != answer
+    if /[^0-9]/.match(guess)
+      wrong_data(max)
+      guess = question_chomp(question)
+    else
+      guess = guess.to_i
+      if guess >= (max + 1) || guess < 0
+        wrong_data(max)
+        guess = question_chomp(question)
+      elsif guess < answer
+        print "too low, try again..."
+        guess = question_chomp(question)
+      elsif guess > answer
+        print "too high, try again..."
+        guess = question_chomp(question)
+      elsif guess == answer
+        print "Congratulations, you guessed the number."
+        break
+      end
     end
   end
+end
 
+def question_chomp(question)
+  print question
+  gets.chomp
+end
+
+def wrong_data(max)
+  puts "Invalid input, must enter a number between 0 and #{max}."
+end
 can_guess_it
